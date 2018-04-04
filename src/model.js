3ca1ba5c8dc627d06837d5f364d33854f9a93f58
OriStakhiv <orysiastakh@gmail.com>
2018-04-05 00:38:32 +0300

    something new

---
+++ src/model.js
@@ -0,0 +1,18 @@
+class Model {
+  constructor(state = []) {
+    this.state = state;
+
+  }
+  getItem(id){
+    return this.state.find(item => item.id == id);
+  }
+  addItem(item){
+    this.state.push(item);
+  }
+  updateItem(id, date){
+    const item = this.getItem(id);
+    Object.keys(data).forEach(prop => item[prop] = data[prop]);
+
+  }
+  re
+}
