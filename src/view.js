42791b3e387da9a65150b07e0b37baa02c28ce7d
OriStakhiv <orysiastakh@gmail.com>
2018-04-06 14:37:09 +0300

    need to completed method

--- src/view.js
+++ src/view.js
@@ -1,9 +1,26 @@
+import {createElement} from './helpers.js';
 class View {
   constructor() {
     this.form = document.getElementById('todo-form');
     this.input = document.getElementById('add-input');
     this.list = document.getElementById('todo-list');
 
+  }
+
+  createElement(todo){
+    const checkbox = createElement('input', {type: 'checkbox', className: 'checkbox', checked: todo.completed ? 'checked' : ''});
+    const label = createElement('label', {className: 'title'}, todo.title);
+    const editInput= createElement('input', {type: 'text', className: 'textfield'});
+    const editButton = createElement('button', {className: 'edit'}, 'Edit');
+    const removeButton = createElement('button', {className: 'remove'}, 'Delete');
+    const item = createElement('li', {className: 'todo-item${todo.completed ? ' completed' : ''}', 'data-id': todo.id }, checkbox, label, editInput, editButton, removeButton);
+    return this.addEventListeners(item);
+
+
+  }
+
+  addEventListeners(item){
+    
   }
   findListItem(id){
     return this.list.querySelector('[data-id="${id}"]')
@@ -40,7 +57,11 @@ class View {
 
   }
   removeItem(id){
-    
+
+    const listItem = this.findListItem(todo.id);
+    this.list.removeChild(listItem);
+
   }
 }
+
 export default View;
