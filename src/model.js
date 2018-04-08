3f92507d692903d133330e769e7773f72c853ca9
OriStakhiv <orysiastakh@gmail.com>
2018-04-08 21:09:15 +0300

    model ready

--- src/model.js
+++ src/model.js
@@ -1,4 +1,4 @@
-import {EventEmitter} from './helpers';
+import { EventEmitter } from './helpers';
 
 class Model extends EventEmitter {
   constructor(items = []) {
@@ -9,27 +9,38 @@ class Model extends EventEmitter {
         //  title: '',
         //  completed: false
         //}
-
-
   }
   getItem(id){
     return this.items.find(item => item.id == id);
   }
+
   addItem(item){
+
     this.items.push(item);
+    this.emit('change',this.items);
+
     return item;
-  }
-  updateItem(id, date){
-    const item = this.getItem(id);
-    Object.keys(data).forEach(prop => item[prop] = data[prop]);
-    return item;
-  }
-  removeItem(id){
-    const index = this.items.findIndex(item => item.id == id);
-    if (index > -1) {
-      this.items.splice(index, 1);
-    }
   }
 
+  updateItem(id, data){
+
+    const item = this.getItem(id);
+
+    Object.keys(data).forEach(prop => item[prop] = data[prop]);
+    this.emit('change', this.items);
+
+    return item;
+  }
+
+  removeItem(id){
+
+    const index = this.items.findIndex(item => item.id == id);
+
+    if (index > -1) {
+      this.items.splice(index, 1);
+      this.emit('change', this.items);
+    }
+  }
 }
+
 export default Model;
